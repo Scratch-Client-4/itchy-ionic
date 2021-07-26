8a426c9dad91595f36e8006d51a8798405009961
Micah Lindley <micahincorporated@gmail.com>
2021-07-26 08:54:54 -0500

    ♿️ Improve legibility of FAB text in projects

--- src/components/ProjectModal.vue
+++ src/components/ProjectModal.vue
@@ -281,7 +281,7 @@ ion-fab-button[data-desc]::after {
   background-color: var(--ion-color-light);
   padding: 9px;
   border-radius: 5px;
-  color: white;
+  color: var(--ion-text-color);
   box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12);
 }
 </style>
