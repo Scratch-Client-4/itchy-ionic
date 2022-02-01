c100efd58ebe76cfd9962cf6bc74abb7eb1d0e81
Micah Lindley <micahincorporated@gmail.com>
2022-01-31 22:40:00 -0600

    ✨ Add projects to user page

--- src/components/UserModal.vue
+++ src/components/UserModal.vue
@@ -5,7 +5,13 @@
       :scroll-events="true"
       @ionScroll="scroll($event)"
     >
-      <ion-progress-bar v-if="loading" type="indeterminate"></ion-progress-bar>
+      <transition name="opacity">
+        <ion-progress-bar
+          v-if="loading"
+          type="determinate"
+          :value="loadingStatus / 6"
+        />
+      </transition>
       <div class="backbutton" :style="{ background: headerColor }">
         <ion-back-button
           default-href="explore"
@@ -48,6 +54,7 @@
       </ion-card>
       !-->
       <ProjectCard
+        class="featured-project"
         :title="featuredProject.title"
         :author="featuredProject.author"
         :thumb="featuredProject.thumbnail"
@@ -73,6 +80,40 @@
         </ion-card-content>
         <div class="shadow" v-if="selected != 'wiwo'"></div>
       </ion-card>
+      <div v-if="!loading">
+        <ion-text>
+          <h3>Projects ({{ projectCount }})</h3>
+        </ion-text>
+        <div class="sidescroll">
+          <div v-for="project in userProjects" :key="project.id">
+            <ProjectCard
+              class="user-projects"
+              :title="project.title"
+              :author="username"
+              :thumb="project.image"
+              :id="project.id"
+              :instructions="project.instructions"
+              :credits="project.description"
+            ></ProjectCard>
+          </div>
+        </div>
+        <ion-text>
+          <h3>Favorites</h3>
+        </ion-text>
+        <div class="sidescroll">
+          <div v-for="project in favoriteProjects" :key="project.id">
+            <ProjectCard
+              class="user-projects"
+              :title="project.title"
+              :author="project.author.username"
+              :thumb="project.image"
+              :id="project.id"
+              :instructions="project.instructions"
+              :credits="project.description"
+            ></ProjectCard>
+          </div>
+        </div>
+      </div>
       <ion-fab vertical="bottom" horizontal="end" slot="fixed">
         <ion-fab-button>
           <ion-spinner v-if="opening" />
@@ -129,6 +170,7 @@ export default defineComponent({
       backgroundColor: "#303c54",
       textColor: "white",
       loading: true,
+      loadingStatus: 0,
       activity: [],
       bio: {
         wiwo: "",
@@ -140,6 +182,9 @@ export default defineComponent({
       showHeader: false,
       headerColor: "transparent",
       opening: false,
+      projectCount: "loading",
+      userProjects: [],
+      favoriteProjects: [],
       exitOutline,
       personAdd,
       calendar,
@@ -199,12 +244,13 @@ export default defineComponent({
     },
     async loadUser() {
       let user,
-        userActivity = {},
-        loadingStatus = 0;
+        userActivity = {};
+      this.loadingStatus = 0;
       const isLoading = () => {
-        console.log(loadingStatus);
-        if (loadingStatus > 2) {
-          this.loading = false;
+        if (this.loadingStatus == 6) {
+          setTimeout(() => {
+            this.loading = false;
+          }, 300);
           return false;
         } else {
           return true;
@@ -224,15 +270,43 @@ export default defineComponent({
       });
       Http.request({
         method: "GET",
+        url: `https://api.scratch.mit.edu/users/${this.username}/favorites?limit=12`,
+      }).then((res) => {
+        this.loadingStatus++;
+        res.data.forEach((project) => {
+          Http.request({
+            method: "GET",
+            url: `https://api.scratch.mit.edu/projects/${project.id}`,
+          }).then((res) => {
+            if (!res.error) {
+              project.author.username = res.data.author.username;
+              this.favoriteProjects.push(project);
+            }
+          });
+        });
+        this.loadingStatus++;
+        isLoading();
+      });
+      Http.request({
+        method: "GET",
         url: `https://itchy-api.vercel.app/api/user?user=${this.username}`,
       }).then((res) => {
-        loadingStatus++;
+        this.loadingStatus++;
         isLoading();
         user = res.data;
         this.bio.about = user.profile.bio;
         this.bio.wiwo = user.profile.status;
+        this.projectCount = user.projectCount;
         let fake = document.createElement("img");
         fake.src = `https://cdn2.scratch.mit.edu/get_image/user/${user.id}_500x500.png`;
+        Http.request({
+          method: "GET",
+          url: `https://api.scratch.mit.edu/users/${this.username}/projects`,
+        }).then((res) => {
+          this.userProjects = res.data;
+          this.loadingStatus++;
+          isLoading();
+        });
         fake.onload = () => {
           /*
           Http.request({
@@ -253,7 +327,7 @@ export default defineComponent({
               this.bio.wiwo = userStats.work;
               this.bio.about = userStats.bio;*/
           this.pfp = `https://cdn2.scratch.mit.edu/get_image/user/${user.id}_500x500.png`;
-          loadingStatus++;
+          this.loadingStatus++;
           isLoading();
           /*});
           })*/
@@ -270,7 +344,7 @@ export default defineComponent({
         url: `https://scratch.mit.edu/messages/ajax/user-activity/?user=${this.username}&max=6`,
       }).then((res) => {
         console.log(userActivity);
-        loadingStatus++;
+        this.loadingStatus++;
         isLoading();
         let unparsed = res.data;
         unparsed = parse(unparsed);
@@ -325,7 +399,7 @@ ion-card-title {
   font-size: 2.5em;
 }
 
-::v-deep(ion-card) {
+::v-deep(ion-card.featured-project) {
   width: 98%;
   margin: auto;
   max-height: 100% !important;
@@ -418,6 +492,16 @@ p {
   transform: translateY(20%);
 }
 
+.opacity-enter-active,
+.opacity-leave-active {
+  transition: opacity 0.2s ease-out;
+}
+
+.opacity-enter-from,
+.opacity-leave-to {
+  opacity: 0;
+}
+
 h1 {
   margin-top: 0;
   margin-bottom: 0.1em;
@@ -427,11 +511,19 @@ ion-progress-bar {
   z-index: 1;
 }
 
-ion-card {
+ion-card.text-box {
   z-index: -1;
   max-height: 18vh;
   transition: max-height 0.3s ease-out;
   margin-bottom: 0.5em;
+  width: 98%;
+  margin-left: 4px;
+}
+
+.sidescroll {
+  width: 100vw;
+  margin-left: -16px;
+  padding: 0 16px;
 }
 
 .shadow {
@@ -451,5 +543,20 @@ ion-card {
 ion-card.selected {
   max-height: 900% !important;
   transition: max-height 0.3s ease-out;
+}
+
+.text-box:last-of-type {
+  margin-bottom: 1.5em;
+}
+
+h3 {
+  margin-left: 10px;
+}
+
+.project-grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto auto;
+  margin: 10px;
 }
 </style>
