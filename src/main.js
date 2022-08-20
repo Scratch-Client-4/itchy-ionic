968da397900d1e42840bf664a7cd1e97f905c9f9
Locness <locness3@e.email>
2022-08-20 23:12:59 +0200

    [WIP - Localization] Add i18next setup

--- src/main.js
+++ src/main.js
@@ -11,6 +11,11 @@ import {
   IonicVue
 } from '@ionic/vue';
 
+import i18next from "i18next";
+import i18NextVue from "i18next-vue";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
 
@@ -32,10 +37,28 @@ import './theme/variables.css';
 import './theme/custom.css';
 import VueSafeHTML from 'vue-safe-html';
 
+// https://www.i18next.com/how-to/add-or-load-translations
+// https://dev.to/adrai/how-to-properly-internationalize-a-vue-application-using-i18next-1doj
+i18next
+.use(Backend)
+.use(LanguageDetector)
+.init({
+  backend: {
+    loadPath: "/locales/{{lng}}/{{ns}}.json"
+  },
+  fallbackLng: "en",
+  // keep these in alphebatical order
+  ns: ["account", "comments", "common", "explore", "messages", "navigation",
+  "projects", "search", "settings", "studios", "users"],
+  derfaultNS: "common"
+})
+
+// https://github.com/i18next/i18next-vue#readme
 const app = createApp(App)
   .use(IonicVue)
   .use(router)
-  .use(VueSafeHTML);
+  .use(VueSafeHTML)
+  .use(i18NextVue, { i18next });
 
 router.isReady().then(() => {
   app.mount('#app');
